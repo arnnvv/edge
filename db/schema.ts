8685796922596b0a7dbf79ec169d7105a1af9075
arnnvv <arnav.900@protonmail.com>
2024-06-12 11:59:27 +0530

    scheema

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,26 @@
-import { sqliteTable, integer } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const items = sqliteTable("items", {
-  id: integer("id").notNull().primaryKey(),
+export const chat = sqliteTable("chats", {
+  id: text("id").notNull().primaryKey(),
+  name: text("name").notNull(),
+  createdAt: text("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
 });
+
+export type Chat = typeof chat.$inferSelect;
+
+export const message = sqliteTable("messages", {
+  id: integer("id").notNull().primaryKey(),
+  chatId: text("chat_id")
+    .notNull()
+    .references(() => chat.id),
+  role: text("role", { enum: ["user", "assistant"] }).notNull(),
+  content: text("content").notNull(),
+  createdAt: text("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+});
+
+export type Message = typeof message.$inferSelect;
